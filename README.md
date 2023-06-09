4bac43245e1c381abbd44e8f0307af0b8669d437
Abev <31078916+Abev08@users.noreply.github.com>
2023-06-09 17:11:16 +0200

    v1.0.1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Simple browser extension that allows volume control of audio and video elements in browser tabs.
+Simple mozilla browsers extension that allows volume control of audio and video elements in browser tabs.
 
 Tested with Firefox 114.0
+Tested on websites / functions: youtube, youtube shorts, twitch, 9gag, instagram, instagram stories
+
+<br><br>
+[CHANGELOG](./changelog.txt "CHANGELOG")
--- content.js
+++ content.js
@@ -1,9 +1,11 @@
+let setAtLeastOnce = false;
 let tabVol = 1.0;
 
 let lastScrollPosition = 0;
 document.addEventListener("scroll", (event) => {
   if (Math.abs(window.scrollY - lastScrollPosition) >= 500) {
     lastScrollPosition = window.scrollY;
+    if (!setAtLeastOnce) getVolume();
     setVolume(tabVol);
   }
 });
@@ -12,8 +14,7 @@ browser.runtime.onMessage.addListener((message) => {
   if (message.extension == "volumeControl") {
     if (message.action == "setVolume") setVolume(message.value);
     else if (message.action = "getVolume") {
-      elements = document.querySelectorAll("video, audio");
-      if (elements.length == 1) tabVol = elements[0].volume;
+      getVolume();
       return Promise.resolve({ response: tabVol });
     };
   }
@@ -22,5 +23,32 @@ browser.runtime.onMessage.addListener((message) => {
 function setVolume(value) {
   tabVol = value;
   elements = document.querySelectorAll("video, audio");
-  for (element of elements) { element.volume = value; }
+  elements.forEach((element) => {
+    element.volume = value;
+    // Try catch everywhere because not every website allows adding listeners to video / audio elements?
+    // Attach listeners when document content changed to update new video and audio controls
+    // Youtube shorts - assign new video previously set tab volume
+    try { element.addEventListener("loadedmetadata", (event) => { event.target.volume = tabVol; }); }
+    catch { } // Do nothing?
+    // Instagram stories - previous video ended and new is loaded
+    try { element.addEventListener("emptied", (event) => { setVolume(tabVol) }); }
+    catch { } // Do nothing?
+  });
+  setAtLeastOnce = true;
+}
+
+function getVolume() {
+  elements = document.querySelectorAll("video, audio");
+  if (elements.length > 0) {
+    if (!setAtLeastOnce) tabVol = elements[0].volume; // Assume that first element on website has correct volume
+    elements.forEach((element) => {
+      // Try catch everywhere because not every website allows adding listeners to video / audio elements?
+      // Standard volume change event on player - set volume remembered by extension from volume change on player
+      try { element.addEventListener.addEventListener("volumechange", (event) => { tabVol = event.target.volume; }); }
+      catch { } // Do nothing?
+    });
+
+    setVolume(tabVol);
+  }
+  setAtLeastOnce = true;
 }
