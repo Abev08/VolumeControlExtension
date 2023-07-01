b2e7ececf3076bebf479a473703b1b50a1b0e5b9
Abev <31078916+Abev08@users.noreply.github.com>
2023-07-01 09:22:32 +0200

    v1.0.3

--- content.js
+++ content.js
@@ -22,38 +22,53 @@ browser.runtime.onMessage.addListener((message) => {
 
 function setVolume(value, updateMutedFlag = false) {
   tabVol = parseFloat(value).toFixed(2);
-  elements = document.querySelectorAll("video, audio, [id^=player-volume-slider], [class^=ytp-volume-panel]");
+  elements = document.querySelectorAll("video, audio, [id=movie_player], [id^=player-volume-slider], [class^=ytp-volume-panel]");
   elements.forEach((element) => {
     if (element.tagName == "VIDEO" || element.tagName == "AUDIO") {
-      element.volume = value;
+      element.volume = tabVol;
       // Unmute / mute only elements that are not paused - I don't want to unmute something somewhere in the page that shouldn't be playing
       if (updateMutedFlag && !element.paused) {
         // Set muted if volume is set to 0
-        if (value == 0) element.muted = true;
+        if (tabVol == 0) element.muted = true;
         else element.muted = false;
       }
       // Try catch everywhere because not every website allows adding listeners to video / audio elements?
       // Attach listeners when document content changed to update new video and audio controls
-      // Youtube shorts - assign new video previously set tab volume
+      // YouTube shorts - assign new video previously set tab volume
       try { element.addEventListener("loadedmetadata", (event) => { event.target.volume = tabVol; }); }
-      catch { } // Do nothing?
+      catch { }
       // Instagram stories - previous video ended and new is loaded
       try { element.addEventListener("emptied", (event) => { setVolume(tabVol) }); }
-      catch { } // Do nothing?
+      catch { }
+    } else if (element.id == "movie_player") {
+      // YouTube stream
+      try {
+        // Something about extension not running in the same enviroment as YouTube API and not working,
+        // the solution is to inject javascript into the page and run it there
+        var script = document.createElement("script");
+        var code = `document.getElementById("movie_player").setVolume(` + (tabVol * 100) + `);`;
+        script.appendChild(document.createTextNode(code));
+        document.querySelector("head").appendChild(script);
+        script.remove();
+        // It would be a lot easier if code below would work
+        // element.setVolume(tabVol);
+        // if (updateMutedFlag && element.getPlayerState() == 1) { // getPlayerState() == 1 playing, == 2 paused ??
+        //   // Set muted if volume is set to 0
+        //   if (tabVol == 0) element.mute();
+        //   else element.unMute();
+        // }
+      } catch { }
     } else {
       // Try to set value of volume sliders
       try {
-        element.setAttribute("data-a-visible", true);
-        element.setAttribute("aria-valuenow", Math.floor(value * 100));
-        element.setAttribute("aria-valuetext", Math.floor(value * 100) + "%");
-        element.setAttribute("value", value);
-        element.setAttribute("data-a-visible", false);
+        element.setAttribute("aria-valuenow", Math.floor(tabVol * 100));
+        element.setAttribute("aria-valuetext", Math.floor(tabVol * 100) + "%");
+        element.setAttribute("value", tabVol);
+        element.value = tabVol;
       }
-      catch (ex) { console.log(ex); }
+      catch { }
     }
   });
-  elements = document.querySelectorAll("video, audio");
-
   setAtLeastOnce = true;
 }
 
