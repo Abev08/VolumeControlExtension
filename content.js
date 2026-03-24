4f7195f3932303213e85c2568a9b3d7f5c4803de
Abev08 <31078916+Abev08@users.noreply.github.com>
2026-03-24 19:20:54 +0100

    v1.0.7

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 let setAtLeastOnce = false;
 let tabVol = 1.0;
 let mediaElements = new Array();
+let extensionOpenedAtLeastOnce = false;
 
 browser.runtime.onMessage.addListener((message) => {
   if (message.extension == "volumeControl") {
@@ -10,13 +11,14 @@ browser.runtime.onMessage.addListener((message) => {
         break;
 
       case "getVolume":
+        extensionOpenedAtLeastOnce = true;
         getVolume();
         return Promise.resolve({ response: tabVol });
     }
   }
 });
 
-function attachVolumeChangeEvents() {
+const observer = new MutationObserver(mutations => {
   // Clear previously added volume change events
   while (mediaElements.length > 0) {
     const element = mediaElements.pop();
@@ -24,51 +26,56 @@ function attachVolumeChangeEvents() {
   }
 
   // Add new events
+  attachVolumeChangeEvents();
+  setVolume(tabVol);
+});
+observer.observe(document.body, { childList: true, subtree: true });
+
+function attachVolumeChangeEvents() {
   document.querySelectorAll("video, audio").forEach(element => {
     element.addEventListener("volumechange", updateTabVolumeFromElement);
+    mediaElements.push(element);
   });
 }
 
 function updateTabVolumeFromElement(event) {
   if (!event.target.muted && event.eventPhase != 0) {
-    tabVol = parseFloat(event.target.volume).toFixed(3);
+    setVolume(event.target.volume, false, event.target);
   }
 }
 
-attachVolumeChangeEvents();
-
-const observer = new MutationObserver(() => {
-  attachVolumeChangeEvents();
-  if (!setAtLeastOnce) getVolume();
-  setVolume(tabVol);
-});
-observer.observe(document.body, { childList: true, subtree: true });
-
-function setVolume(value, updateMutedFlag = false) {
+function setVolume(value, updateMutedFlag = false, skipElement = undefined) {
   tabVol = parseFloat(value).toFixed(3);
 
-  elements = document.querySelectorAll("video, audio");
-  elements.forEach(element => {
-    element.volume = tabVol;
-    if (updateMutedFlag && !element.paused) {
-      if (tabVol == 0) { element.muted = true; }
-      else { element.muted = false; }
+  // Skip volume changes if the extension was not opened at least once
+  if (!extensionOpenedAtLeastOnce) { return; }
+  mediaElements.forEach(element => {
+    if (element == skipElement) {
+      // Skip
+    } else {
+      element.volume = tabVol;
+      if (updateMutedFlag && !element.paused) {
+        if (tabVol == 0) { element.muted = true; }
+        else { element.muted = false; }
+      }
+      element.dispatchEvent(new Event("volumechange"));
     }
-    element.dispatchEvent(new Event("volumechange"));
   });
 
   setAtLeastOnce = true;
 }
 
 function getVolume() {
-  elements = document.querySelectorAll("video, audio");
-  if (elements.length > 0) {
+  if (mediaElements.length == 0) {
+    attachVolumeChangeEvents();
+  }
+
+  if (mediaElements.length > 0) {
     if (!setAtLeastOnce) {
       // Assume that first element on the website has correct volume
-      tabVol = parseFloat(elements[0].volume).toFixed(3);
+      tabVol = parseFloat(mediaElements[0].volume).toFixed(3);
     }
 
     setVolume(tabVol);
   }
-  setAtLeastOnce = true;
 }
